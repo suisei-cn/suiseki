412cb3694c99c803bc995498014191aedfd83c88
Outvi V <19144373+outloudvi@users.noreply.github.com>
2024-02-05 12:02:30 +0000

    chore: offboard Sentry and update CI deps

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
 import App from './App.svelte'
 import initi18n from './utils/i18n'
-import * as Sentry from '@sentry/browser'
 
 initi18n()
-
-Sentry.init({
-  dsn:
-    'https://96cc0ec9a8e54ce2b4599eb4ef5f1df3@o407537.ingest.sentry.io/5438158',
-  release: 'NULL_RELEASE',
-})
 
 const app = new App({
   target: document.body,
